6fcaad13374cd6e5ba47c8d9de8e248974398620
Felipe Sebben <felipesebben@yahoo.com.br>
2024-02-05 16:47:28 -0300

    feat: adicionar docstrings nos testes funcionais

--- tests/test_funcional.py
+++ tests/test_funcional.py
@@ -4,17 +4,22 @@ import pytest
 import subprocess
 from selenium.webdriver.common.by import By
 import os
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 @pytest.fixture
 def driver():
+    """
+    Função para inicializar e gerenciar o WebDriver para testes funcionais. Após a execução dos testes, o WebDriver é fechado.
+    
+    Retorna:
+        `webdriver.Chrome`: Uma instância do WebDriver do Chrome.
+    """
     # Iniciar o Streamlit em background
     process = subprocess.Popen(["streamlit", "run", "src/app.py"])
     options = Options()
     options.headless = True  # Executar em modo headless
-    driver = webdriver.Firefox(options=options)
-    # Iniciar o WebDriver usando GeckoDriver
+    driver = webdriver.chrome(options=options)
     driver.set_page_load_timeout(5)
     yield driver
 
@@ -23,12 +28,17 @@ def driver():
     process.kill()
 
 def test_app_opens(driver):
+    """
+    Testa a abertura da página do Streamlit.
+    """
     # Verificar se a página abre
     driver.get("http://localhost:8501")
     sleep(2)
 
 def test_check_title_is(driver):
-    # Verificar se a página abre
+    """
+    Testa se o título da página é o esperado. Captura o título da página e verifica se é igual ao esperado. Caso contrário, o teste falha.
+    """
     driver.get("http://localhost:8501")
     # Verifica se o titulo de página é
     sleep(2)
@@ -40,6 +50,9 @@ def test_check_title_is(driver):
     assert page_title == expected_title
 
 def test_check_streamlit_h1(driver):
+    """
+    Testa se o texto do elemento <h1> é o esperado. Captura o texto do elemento <h1> e verifica se é igual ao esperado. Caso contrário, o teste falha.
+    """
     # Acessar a página do Streamlit
     driver.get("http://localhost:8501")
 
@@ -54,6 +67,9 @@ def test_check_streamlit_h1(driver):
     assert h1_element.text == expected_text
 
 def test_check_usuario_pode_inserir_um_excel_e_receber_uma_mensagem(driver):
+    """
+    Testa se o usuário pode inserir um arquivo Excel e receber uma mensagem de sucesso. Insere um arquivo Excel válido e verifica se a mensagem de sucesso é exibida.
+    """
     # Acessar a página do Streamlit
     driver.get("http://localhost:8501")
 
@@ -69,6 +85,11 @@ def test_check_usuario_pode_inserir_um_excel_e_receber_uma_mensagem(driver):
     assert "O schema do arquivo Excel está correto!" in driver.page_source
 
 def test_check_mais_de_uma_mensagem_de_erro(driver):
+    """
+    Testa se o usuário pode inserir um arquivo Excel inválido e receber mais de uma mensagem de erro. 
+    Localiza todas as ocorrências de erro, verifica se há pelo menos duas mensagens de erro. 
+    Insere um arquivo Excel inválido e verifica se mais de uma mensagem de erro é exibida.
+    """
     # Acessar a página do Streamlit
     driver.get("http://localhost:8501")
 
@@ -88,6 +109,9 @@ def test_check_mais_de_uma_mensagem_de_erro(driver):
     assert len(error_messages) == 2, f"Quantidade de mensagens de erro encontradas: {len(error_messages)}"
 
 def test_check_usuario_insere_um_excel_valido_e_aparece_um_botao(driver):
+    """
+    Testa se o usuário pode inserir um arquivo Excel válido e receber um botão para salvar no banco de dados.
+    """
     # Acessar a página do Streamlit
     driver.get("http://localhost:8501")
 
