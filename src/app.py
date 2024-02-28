c92597ecd223c344da959aecfac3e3bb1049f737
Felipe Sebben <felipesebben@yahoo.com.br>
2024-02-28 11:41:06 -0300

    feat: push to test branch protection

--- src/app.py
+++ src/app.py
@@ -14,7 +14,7 @@ def main():
 
         if error:
             ui.display_wrong_message()
-            logging.error("Planilha apresentava erro de schema.")
+            logging.error("Planilha apresenta erro de schema.")
         elif ui.display_save_button():
             ui.display_success_message()
             logging.info("Planilha validada e salva no banco de dados.")
