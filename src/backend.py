2165bd2f28c04830284334c1d6f7888cfbeaef9b
Felipe Sebben <felipesebben@yahoo.com.br>
2024-03-06 11:19:52 -0300

    refactor: format code

--- src/backend.py
+++ src/backend.py
@@ -1,22 +1,26 @@
-import pandas as pd
-from contrato import Vendas
-from dotenv import load_dotenv
 import os
+
+import pandas as pd
+from dotenv import load_dotenv
+
+from contrato import Vendas
+
 
 class ExcelProcessor:
     """
     Classe para processar o arquivo Excel.
     """
+
     def __init__(self):
         # Carrega as variáveis de ambiente
         load_dotenv(".env")
 
         # Lê as variáveis de ambiente
-        self.POSTGRES_USER = os.getenv('POSTGRES_USER')
-        self.POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
-        self.POSTGRES_HOST = os.getenv('POSTGRES_HOST')
-        self.POSTGRES_PORT = os.getenv('POSTGRES_PORT')
-        self.POSTGRES_DB = os.getenv('POSTGRES_DB')
+        self.POSTGRES_USER = os.getenv("POSTGRES_USER")
+        self.POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+        self.POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+        self.POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+        self.POSTGRES_DB = os.getenv("POSTGRES_DB")
 
         # Cria a URL de conexão com o banco de dados
         self.DATABASE_URL = f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
@@ -28,7 +32,10 @@ class ExcelProcessor:
             # Verificar se há colunas extras no DataFrame
             extra_cols = set(df.columns) - set(Vendas.model_fields.keys())
             if extra_cols:
-                return False, f"Colunas extras detectadas no Excel: {', '.join(extra_cols)}"
+                return (
+                    False,
+                    f"Colunas extras detectadas no Excel: {', '.join(extra_cols)}",
+                )
 
             # Validar cada linha com o schema escolhido
             for index, row in df.iterrows():
@@ -43,7 +50,7 @@ class ExcelProcessor:
             return df, False, str(ve)
         except Exception as e:
             return df, False, f"Erro inesperado: {str(e)}"
-        
+
     def save_dataframe_to_sql(self, df):
         # Salva o DataFrame no banco de dados
-        df.to_sql('vendas', con=self.DATABASE_URL, if_exists='replace', index=False)
+        df.to_sql("vendas", con=self.DATABASE_URL, if_exists="replace", index=False)
